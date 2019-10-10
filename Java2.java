1e99941ea2c6c9586e53415b582fd10ae7ad3cb0
NurijeHa <nurijehalitii@gmail.com>
2019-10-10 14:50:06 +0200

    java2

---
+++ Java2.java
@@ -0,0 +1,20 @@
+public class Java2{
+	public static void main(String[] args) {
+       
+        
+		int n = 5;
+		int m = 6;
+		
+		if(n < m )
+		{
+			System.out.println("N < M ");
+		}
+		else
+		{
+			System.out.println("N>M");
+		}
+		
+		System.out.println(n);
+	}
+}
+		
