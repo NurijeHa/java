8a2322cd0b3d063ec6315ad3014f914eedb91b7b
NurijeHa <nurijehalitii@gmail.com>
2019-10-10 14:04:16 +0200

    test22

--- java.java
+++ java.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
 public class Java{
 	public static void main(String[] args) {
 	
-	int n=5;
-	int j=4;
-	int suma;
+	int num1, num2, suma, zb, pj, sh;
 	
-	suma = n + j;
-	System.out.println("Suma is", suma);
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter number first: ");
+	num1 = sc.nextInt();
+	
+	System.out.println("Enter number second: ");
+	num2 = sc.nextInt();
+	sc.close();
+	
+	
+	suma = num1 + num2;
+	zb = num1 - num2;
+	pj = num1 / num2;
+	sh = num1 * num2;
+	
+	System.out.println("Suma is: "+ suma);
+	System.out.println("Zb is: " + '/n'  + zb);
+	System.out.println("Pj is" + pj);
+	System.out.println("Sh is" + sh);
+
 	}
 	
 }
