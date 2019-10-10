3c4cd03d9b640462a73693a87d734780ed708a4f
NurijeHa <nurijehalitii@gmail.com>
2019-10-10 17:29:16 +0200

    Java3 test

--- Java3.java
+++ Java3.java
@@ -1,10 +1,12 @@
 public class Java3{
 	public static void main(String[] args) {
 		
-		String name = " Nurije ";
+		String name = " Nurije";
 		String surname = "Haliti";
+		int ages = 23;
 		
-		System.out.println("My name is: " + name);
-		System.out.println("My surname is: " + surname);
+		System.out.println("My name is: " + name + ".");
+		System.out.println("My surname is: " + surname + ".");
+		System.out.println("I am: " + ages + " years old.");
 	}
 }
