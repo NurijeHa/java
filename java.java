6238a614b7ba826c9642566cad070f07993df2ab
NurijeHa <nurijehalitii@gmail.com>
2019-10-11 14:43:33 +0200

    test test test

--- java.java
+++ java.java
@@ -5,10 +5,10 @@ public class Java{
 	int num1, num2, suma, zb, pj, sh;
 	
 	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter number first: ");
+	System.out.println("Enter number first:  ");
 	num1 = sc.nextInt();
 	
-	System.out.println("Enter number second: ");
+	System.out.println("Enter number second:  ");
 	num2 = sc.nextInt();
 	sc.close();
 	
@@ -18,10 +18,10 @@ public class Java{
 	pj = num1 / num2;
 	sh = num1 * num2;
 	
-	System.out.println("Suma is: "+ suma);
-	System.out.println("Zb is: " + '/n'  + zb);
-	System.out.println("Pj is" + pj);
-	System.out.println("Sh is" + sh);
+	System.out.println("Suma is: " + suma);
+	System.out.println("Zb is: "  + zb);
+	System.out.println("Pj is: " + pj);
+	System.out.println("Sh is: " + sh);
 
 	}
 	
