aa69514f927698957de87abf4746e1c88fd2db23
NurijeHa <nurijehalitii@gmail.com>
2019-10-11 14:17:51 +0200

    to add data

--- Java4.java
+++ Java4.java
@@ -1,21 +1,50 @@
+import java.util.Scanner;
 public class Java4 {
 	public static void main(String[] args) {
 		
-		int id = 1;
-		String name = "Nurije";
-		String surname = "Haliti";
-		int age = 23;
-		String dataBirth = "25.12.1995";
-		String address = "Arberi, Gjilan";
-		String country = "Kosovo";
-		String nationality = "Kosovr";
-		String faculty = "Computer Science";
 		
+		int id = 1;
+		String name;
+		String surname;
+		int age;
+		String address;
+		String country;
+		String nationality ;
+		String faculty ;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your Name: ");
+		name = sc.nextLine();
+		
+	
+		System.out.println("Enter your Surname: ");
+		surname = sc.nextLine();
+		
+		
+		System.out.println("Enter your Address: ");
+		address = sc.nextLine();
+		
+			
+		System.out.println("Enter your Country: ");
+		country = sc.nextLine();
+		
+		
+		System.out.println("Enter your Nationality: ");
+		nationality = sc.nextLine();
+		
+		
+		System.out.println("Enter your Faculty: ");
+		faculty = sc.nextLine();
+		
+		
+		System.out.println("Enter your Age: ");
+		age = sc.nextInt();
+		
+		sc.close();
 		
 		System.out.println("ID: " + id);
 		System.out.println("Name: " + name);
 		System.out.println("Surname: " + surname);
-		System.out.println("Date of birth: " + dataBirth);
 		System.out.println("Ages: " + age);
 		System.out.println("Address: " + address);
 		System.out.println("Country: " + country);
