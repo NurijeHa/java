54089bc9b52e93e18b4f40d41d91337568efcee4
NurijeHa <nurijehalitii@gmail.com>
2019-10-11 11:15:16 +0200

    java4 commit

---
+++ Java4.java
@@ -0,0 +1,15 @@
+public class Java4 {
+	public static void main(String[] args) {
+		
+		int id = 1;
+		String name = "Nurije";
+		int age = 23;
+		String address = "Gjilan";
+		
+		
+		System.out.println(id);
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(address);
+	}
+}
